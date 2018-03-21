fc79e54128cc72330f5646cc29d947c27cdcfd7f
Poke1650 <antoine.gagnon760@hotmail.com>
2018-03-21 11:44:28 -0400

    Added the NBTWrapper accessors

--- src/main/java/xyz/poketech/commandscript/sandbox/wrapper/NBTWrapper.java
+++ src/main/java/xyz/poketech/commandscript/sandbox/wrapper/NBTWrapper.java
@@ -1,5 +1,6 @@
 package xyz.poketech.commandscript.sandbox.wrapper;
 
+import com.google.common.collect.ImmutableSet;
 import net.minecraft.nbt.NBTTagCompound;
 
 public class NBTWrapper {
@@ -8,5 +9,58 @@ public class NBTWrapper {
 
     public NBTWrapper(NBTTagCompound tag) {
         this.tag = tag;
+
+        tag.getKeySet();
     }
+
+    public ImmutableSet<String> getKeySet() {
+        return new ImmutableSet.Builder<String>().addAll(tag.getKeySet()).build();
+    }
+    public String getString(String key) {
+        return tag.getString(key);
+    }
+    public boolean getBoolean(String key) {
+        return tag.getBoolean(key);
+    }
+
+    public byte getByte(String key) {
+        return tag.getByte(key);
+    }
+
+    public byte[] getByteArray(String key) {
+        return tag.getByteArray(key);
+    }
+
+    public NBTWrapper getCompoundTag(String key) {
+        return new NBTWrapper(tag.getCompoundTag(key));
+    }
+
+    public double getDouble(String key) {
+        return tag.getDouble(key);
+    }
+
+    public float getFloat(String key) {
+        return tag.getFloat(key);
+    }
+
+    public int getID() {
+        return tag.getId();
+    }
+
+    public int[] getIntArray(String key) {
+        return tag.getIntArray(key);
+    }
+
+    public int getInteger(String key) {
+        return tag.getInteger(key);
+    }
+
+
+
+
+
+
+
+
+
 }
